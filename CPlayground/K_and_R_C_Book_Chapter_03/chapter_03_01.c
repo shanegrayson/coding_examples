f6b072874ee2aa6f267a9f5bb3407bc89de3a7d5
Shane Grayson <shanegrayson@gmail.com>
2019-10-18 14:44:25 -0600

    Stuff

---
+++ CPlayground/K_and_R_C_Book_Chapter_03/chapter_03_01.c
@@ -0,0 +1,13 @@
+/*
+ * chapter_03_01.c
+ *
+ *  Created on: Oct 18, 2019
+ *      Author: Shane
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+void chapter_03_01(char *array) {
+	printf("%s", array);
+}
