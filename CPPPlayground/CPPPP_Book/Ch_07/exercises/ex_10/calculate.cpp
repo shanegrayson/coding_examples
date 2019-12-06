ceaf9a794e30e9db42d3a9296f3982297dfe64ad
Shane Grayson <shanegrayson2001@yahoo.com>
2019-12-05 19:59:50 -0700

    Chapter 7 FINISHEDgit add -A

--- CPPPlayground/CPPPP_Book/Ch_07/exercises/ex_10/calculate.cpp
+++ CPPPlayground/CPPPP_Book/Ch_07/exercises/ex_10/calculate.cpp
@@ -8,25 +8,41 @@
 
 using namespace std;
 
-double calculate(double x, double y, double (*pf)(double, double));
+int const funcs = 3;
+
 double add(double x, double y);
 double sub(double x, double y);
 double mul(double x, double y);
+double calculate(double x, double y, double (*pf)(double, double));
 
 int main(void)
 {
     double x, y, ansAdd, ansSub, ansMul;
+    double (*pa[funcs])(double, double) = { add, sub, mul };
     
     cout << "Please enter an x and y value range(type q to quit): ";
     while (cin >> x >> y)
     {
         /*
+         * Using an array of functions
+         */
+        cout << "START-----AN ARRAY OF POINTED TO FUNCTIONS-----\n\n";
+        auto pb = pa;
+        for (int i = 0; i < funcs; i++)
+            switch (i)
+            {
+                case 0: cout << x << " + " << y << " = " << (*pa[i])(x, y) << endl; break;
+                case 1: cout << x << " - " << y << " = " << (*pa[i])(x, y) << endl; break;
+                case 2: cout << x << " * " << y << " = " << (*pa[i])(x, y) << endl; break;
+            }
+        
+        /*
+         * Using just function poitners
+         */
+        cout << "\nSTART-----JUST POINTED TO FUNCTIONS-----\n\n";
         ansAdd = calculate(x, y, add);
         ansSub = calculate(x, y, sub);
         ansMul = calculate(x, y, mul);
-        */
-
-        
 
         cout << x << " + " << y << " = " << ansAdd << "\n";
         cout << x << " - " << y << " = " << ansSub << "\n";
@@ -40,7 +56,7 @@ int main(void)
 
 double calculate(double x, double y, double (*pf)(double, double))
 {
-    return (*pf)(x, y);
+   return (*pf)(x, y);
 }
 
 
