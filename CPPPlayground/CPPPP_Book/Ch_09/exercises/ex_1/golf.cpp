92b88b906a83273a5e5e4c158891c2bef6d25241
Shane Grayson <shanegrayson@gmail.com>
2020-01-04 23:41:06 -0700

    Added some comments to Ex_1

--- CPPPlayground/CPPPP_Book/Ch_09/exercises/ex_1/golf.cpp
+++ CPPPlayground/CPPPP_Book/Ch_09/exercises/ex_1/golf.cpp
@@ -25,25 +25,28 @@ int main(void)
     handicap(ann, 15);
     showGolf(ann);
 
-    golf members[3];
-
     /*
      * Create multuiple structures calling the overloaded setGolf() 
      * function from golf_func.cpp file.
      * (I think this is a good example of polymorphism.)
      */
+    golf members[3];
     int test = 1;
     int i = 0;
     for (i = 0; i < 3 && test == 1; i++)
     {
-        using namespace std;
+        using std::cout;
+        using std::endl;
+
         test = setGolf(members[i]);
         cout << "Test: " << test << endl;
     }
 
     /*
      * This condition is to set default structs that are not entered
-     * if the user exits early.
+     * if the user exits early. We decrament i by 1 since the test condition
+     * for an empty string is in the function being called, 
+     * so we want to move back by 1 since there will be garbage in the variable
      */
     if (i < 3)
     {
--- CPPPlayground/CPPPP_Book/Ch_09/exercises/ex_1/golf_func.cpp
+++ CPPPlayground/CPPPP_Book/Ch_09/exercises/ex_1/golf_func.cpp
@@ -10,12 +10,17 @@
 #include <iostream>
 #include <cstring>
 
-using std::cout;
-using std::cin;
-using std::endl;
-
+/*
+ * Funciton designed to be interactive with the user.
+ * We ask for a name and handicap. If no name is 
+ * enterd we exit with a status of 0, if there is 
+ * text we exit with 1.
+ */
 int setGolf(golf &g)
 {
+    using std::cin;
+    using std::cout;
+
     cout << "Please enter Golfer's Name: ";
     cin.getline(g.fullName, LEN);
 
@@ -29,19 +34,32 @@ int setGolf(golf &g)
     return 0;
 }
 
+/*
+ * A non-interactive function for setting the 
+ * golf structure.
+ */
 void setGolf(golf &g, const char *name, int hc)
 {
     strcpy(g.fullName, name);
     g.handicap = hc;
 }
 
+/*
+ * Function used to reset the structure handicap
+ */
 void handicap(golf &g, int hc)
 {
     g.handicap = hc;
 }
 
+/*
+ * Print function for the golf structure
+ */
 void showGolf(const golf &g)
 {
+    using std::cout;
+    using std::endl;
+
     cout << "Golfer's Name:-----" << g.fullName << endl;
     cout << "Golfer's Handicap:-" << g.handicap << endl;
 }
